83344cd78ff5b8e61ef972dff78b307930626b43
Allan Feldman <allan.d.feldman@gmail.com>
2019-02-26 19:29:23 -0800

    Change manager outputs.

--- modules/managers/outputs.tf
+++ modules/managers/outputs.tf
@@ -3,9 +3,9 @@ output "ipv4_addresses" {
   description = "The manager nodes public ipv4 adresses"
 }
 
-output "ipv4_addresses_private" {
-  value       = ["${digitalocean_droplet.manager.*.ipv4_address_private}"]
-  description = "The manager nodes private ipv4 adresses"
+output "droplet_ids" {
+  value       = ["${digitalocean_droplet.manager.*.id}"]
+  description = "The droplet ids"
 }
 
 output "manager_token" {
