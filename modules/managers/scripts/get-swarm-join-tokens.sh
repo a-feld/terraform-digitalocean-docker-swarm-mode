ba744c42e2bccd3591529a9fd5084e60216d95ef
Allan Feldman <allan.d.feldman@gmail.com>
2019-02-26 22:24:57 -0800

    Fix swarm token extraction.

--- modules/managers/scripts/get-swarm-join-tokens.sh
+++ modules/managers/scripts/get-swarm-join-tokens.sh
@@ -11,11 +11,11 @@ eval "$(jq -r '@sh "HOST=\(.host)"')"
 
 # Fetch the manager join token
 MANAGER=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-    $USER@$HOST timeout 5 docker swarm join-token manager -q)
+    core@$HOST timeout 5 docker swarm join-token manager -q)
 
 # Fetch the worker join token
 WORKER=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-    $USER@$HOST timeout 5 docker swarm join-token worker -q)
+    core@$HOST timeout 5 docker swarm join-token worker -q)
 
 # Produce a JSON object containing the tokens
 jq -n --arg manager "${MANAGER}" --arg worker "$WORKER" \
