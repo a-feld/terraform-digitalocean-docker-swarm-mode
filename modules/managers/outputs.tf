0a3963ef5a8f43350df9425c0542d2579eddbfbf
Allan Feldman <6374032+a-feld@users.noreply.github.com>
2019-10-13 22:57:22 -0400

    Apply additional terraform 0.12 fixes.

--- modules/managers/outputs.tf
+++ modules/managers/outputs.tf
@@ -1,15 +1,15 @@
 output "ipv4_addresses" {
-  value       = ["${digitalocean_droplet.manager.*.ipv4_address}"]
+  value       = "${digitalocean_droplet.manager.*.ipv4_address}"
   description = "The manager nodes public ipv4 adresses"
 }
 
 output "ipv4_addresses_private" {
-  value       = ["${digitalocean_droplet.manager.*.ipv4_address_private}"]
+  value       = "${digitalocean_droplet.manager.*.ipv4_address_private}"
   description = "The manager nodes private ipv4 adresses"
 }
 
 output "droplet_ids" {
-  value       = ["${digitalocean_droplet.manager.*.id}"]
+  value       = "${digitalocean_droplet.manager.*.id}"
   description = "The droplet ids"
 }
 
